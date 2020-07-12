ac60a2e43ecb756160e361d8e8cffe04fec1d934
GitHub Action <action@github.com>
2020-07-12 08:07:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/nthykier/ideadebpkg/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nthykier/ideadebpkg/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nthykier.ideadebpkg.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nthykier.ideadebpkg.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/nthykier/ideadebpkg/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nthykier/ideadebpkg/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nthykier.ideadebpkg.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nthykier.ideadebpkg.MyBundle
 
 class MyApplicationService {
 
