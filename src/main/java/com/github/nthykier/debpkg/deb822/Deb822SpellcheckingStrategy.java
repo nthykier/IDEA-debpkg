181aa499a941f6398f1d05b860a2b6675693a05e
Niels Thykier <niels@thykier.net>
2020-07-16 17:02:26 +0200

    Improve the spellchecking code for Deb822
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- src/main/java/com/github/nthykier/debpkg/deb822/Deb822SpellcheckingStrategy.java
+++ src/main/java/com/github/nthykier/debpkg/deb822/Deb822SpellcheckingStrategy.java
@@ -5,10 +5,8 @@ import com.github.nthykier.debpkg.deb822.psi.Deb822ValueParts;
 import com.intellij.psi.PsiElement;
 import com.intellij.spellchecker.inspections.PlainTextSplitter;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
-import com.intellij.spellchecker.tokenizer.TokenConsumer;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import com.intellij.spellchecker.tokenizer.TokenizerBase;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -16,16 +14,11 @@ import java.util.Set;
 
 public class Deb822SpellcheckingStrategy extends SpellcheckingStrategy {
 
-    private final Tokenizer<Deb822ValueParts> deb822ValueTokenizer = TokenizerBase.create(PlainTextSplitter.getInstance());
-    private final Tokenizer<Deb822ValueParts> nullTokenizer = new Tokenizer<Deb822ValueParts>() {
-        @Override
-        public void tokenize(@NotNull Deb822ValueParts element, TokenConsumer consumer) {
-
-        }
-    };
     private static final Set<String> SKIP_SPELL_CHECK = new HashSet<>(Arrays.asList(
-            "package", "architecture", "multi-arch", "depends", "recommends", "suggests", "breaks", "replaces",
-            "pre-depends", "build-depends", "build-depends-indep", "build-depends-arch", "provides", "section"));
+            "package", "architecture", "depends", "recommends", "suggests", "breaks", "replaces",
+            "pre-depends", "build-depends", "build-depends-indep", "build-depends-arch", "provides", "section",
+            "testsuite", "maintainer", "uploaders", "source"
+            ));
 
     public Tokenizer getTokenizer(PsiElement element) {
         if (element instanceof Deb822ValueParts) {
@@ -35,11 +28,22 @@ public class Deb822SpellcheckingStrategy extends SpellcheckingStrategy {
             }
             if (parent != null) {
                 String name = ((Deb822FieldValuePair)parent).getField().getText();
-                if (name != null && SKIP_SPELL_CHECK.contains(name.toLowerCase())) {
-                    return nullTokenizer;
+                if (name != null) {
+                    Deb822KnownField knownField = Deb822KnownFieldsAndValues.lookupDeb822Field(name);
+                    /*
+                     * In such a case, it seems awkward to have a spell-check highlight the value when the user cannot
+                     * change the actual value (as it is mandated else where and "correcting" the spelling will cause
+                     * a hard error).
+                     */
+                    if (knownField != null && knownField.hasKnownValues() && knownField.areAllKeywordsKnown()) {
+                        return EMPTY_TOKENIZER;
+                    }
+                    if (SKIP_SPELL_CHECK.contains(name.toLowerCase())) {
+                        return EMPTY_TOKENIZER;
+                    }
                 }
             }
-            return deb822ValueTokenizer;
+            return TEXT_TOKENIZER;
         }
         return super.getTokenizer(element);
     }
