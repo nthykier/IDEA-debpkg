cd2c0c2025c6c95b50457400edf2452000fe1d81
Niels Thykier <niels@thykier.net>
2020-07-18 11:28:33 +0200

    Add Commenter for Deb822 files
    Signed-off-by: Niels Thykier <niels@thykier.net>

---
+++ src/main/java/com/github/nthykier/debpkg/deb822/Deb822Commenter.java
@@ -0,0 +1,31 @@
+package com.github.nthykier.debpkg.deb822;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class Deb822Commenter implements Commenter {
+    @Override
+    public @Nullable String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Override
+    public @Nullable String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
