950cf9b074750f6ccc7728c7cce36801276554ad
Niels Thykier <niels@thykier.net>
2020-07-20 12:32:01 +0200

    Deb822PsiImplUtil: Add some Nullable/NotNull annotations
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- src/main/java/com/github/nthykier/debpkg/deb822/psi/impl/Deb822PsiImplUtil.java
+++ src/main/java/com/github/nthykier/debpkg/deb822/psi/impl/Deb822PsiImplUtil.java
@@ -8,22 +8,24 @@ import com.github.nthykier.debpkg.deb822.psi.Deb822Value;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class Deb822PsiImplUtil {
 
-    public static PsiReference getReference(Deb822Field field) {
+    public static PsiReference getReference(@NotNull Deb822Field field) {
         String fieldName = field.getText();
         Deb822KnownField knownField = Deb822KnownFieldsAndValues.lookupDeb822Field(fieldName);
         return new Deb822FieldPsiReference(field, TextRange.from(0, field.getTextLength()), knownField);
     }
 
-    public static PsiReference getReference(Deb822Substvar substvar) {
+    public static PsiReference getReference(@NotNull Deb822Substvar substvar) {
         String substvarName = substvar.getText();
         Deb822KnownSubstvar knownSubstvar = Deb822KnownSubstvars.lookupSubstvar(substvarName);
         return new Deb822SubstvarPsiReference(substvar, TextRange.from(0, substvar.getTextLength()), knownSubstvar);
     }
 
-    public static PsiReference getReference(Deb822Value value) {
+    public static @Nullable PsiReference getReference(@NotNull Deb822Value value) {
         PsiElement parent = value.getParent();
         while (parent != null && !(parent instanceof Deb822FieldValuePair)) {
             parent = parent.getParent();
