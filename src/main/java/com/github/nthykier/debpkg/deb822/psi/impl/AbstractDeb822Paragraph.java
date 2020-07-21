22c69b7057606b363037178b74cb8420a5bfeb27
Niels Thykier <niels@thykier.net>
2020-07-21 19:14:52 +0200

    Fix parent class of AbstractDeb822Paragraph
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- src/main/java/com/github/nthykier/debpkg/deb822/psi/impl/AbstractDeb822Paragraph.java
+++ src/main/java/com/github/nthykier/debpkg/deb822/psi/impl/AbstractDeb822Paragraph.java
@@ -3,24 +3,15 @@ package com.github.nthykier.debpkg.deb822.psi.impl;
 
 import com.github.nthykier.debpkg.deb822.psi.Deb822FieldValuePair;
 import com.github.nthykier.debpkg.deb822.psi.Deb822Paragraph;
-import com.github.nthykier.debpkg.deb822.psi.Deb822Visitor;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.extapi.psi.StubBasedPsiElementBase;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.util.CachedValue;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.CachedValueImpl;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public abstract class AbstractDeb822Paragraph extends StubBasedPsiElementBase implements Deb822Paragraph {
+public abstract class AbstractDeb822Paragraph extends ASTWrapperPsiElement implements Deb822Paragraph {
 
   private Map<String, Deb822FieldValuePair> fieldMap;
 
