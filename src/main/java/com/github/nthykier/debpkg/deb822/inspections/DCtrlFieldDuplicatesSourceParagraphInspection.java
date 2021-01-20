f3a28474121ddf31b39034acaa00abcb0029efb1
Niels Thykier <niels@thykier.net>
2021-01-20 21:25:21 +0000

    Rename Inspection to end with Inspection
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- src/main/java/com/github/nthykier/debpkg/deb822/inspections/DCtrlFieldDuplicatesSourceParagraphInspection.java
+++ src/main/java/com/github/nthykier/debpkg/deb822/inspections/DCtrlFieldDuplicatesSourceParagraphInspection.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DCtrlFieldDuplicatesSourceParagraph extends LocalInspectionTool {
+public class DCtrlFieldDuplicatesSourceParagraphInspection extends LocalInspectionTool {
 
     public boolean runForWholeFile() {
         return true;
