af471e65b6b69b55962f553a6583522d30b5965e
Niels Thykier <niels@thykier.net>
2022-05-15 17:53:33 +0000

    Remove copy-waste mistake
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- src/main/java/com/github/nthykier/debpkg/deb822/inspections/DCtrlMissingSubstvarsInDependencyFieldsInspection.java
+++ src/main/java/com/github/nthykier/debpkg/deb822/inspections/DCtrlMissingSubstvarsInDependencyFieldsInspection.java
@@ -23,13 +23,8 @@ import java.util.stream.Collectors;
 
 public class DCtrlMissingSubstvarsInDependencyFieldsInspection extends AbstractDctrlInspection {
 
-    private static final LevenshteinDistance EDIT_DISTANCE = new LevenshteinDistance(3);
-
-
     @Override
     protected PsiElementVisitor inspectionVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
-        KnownFieldTable knownFieldTable = Deb822LanguageSupport.fromDeb822Language(holder.getFile().getLanguage()).getKnownFieldTable();
-        List<String> knownFields = knownFieldTable.getAllFieldNames().stream().map(String::toLowerCase).collect(Collectors.toList());
         return new Deb822Visitor() {
             public void visitAllParagraphs(@NotNull Deb822AllParagraphs allParagraphs) {
                 ProgressIndicatorProvider.checkCanceled();
@@ -38,10 +33,6 @@ public class DCtrlMissingSubstvarsInDependencyFieldsInspection extends AbstractD
                     checkAllParagraphs(holder, paragraphs);
                 }
 
-            }
-            public void visitParagraph(@NotNull Deb822Paragraph deb822Paragraph) {
-                ProgressIndicatorProvider.checkCanceled();
-                checkParagraph(holder, deb822Paragraph, knownFieldTable, knownFields);
             }
         };
     }
@@ -78,51 +69,6 @@ public class DCtrlMissingSubstvarsInDependencyFieldsInspection extends AbstractD
                         depends,
                         Deb822Bundle.message("deb822.files.inspection.dctrl-likely-missing-shlibs-depends-in-depends-field.description"),
                         ProblemHighlightType.WARNING
-                );
-            }
-        }
-    }
-
-    private void checkParagraph(ProblemsHolder holder, @NotNull Deb822Paragraph deb822Paragraph, KnownFieldTable knownFieldTable, List<String> knownFieldNames) {
-        SmartList<String> matches = new SmartList<>();
-        for (Deb822FieldValuePair valuePair : deb822Paragraph.getFieldValuePairList()) {
-            Deb822Field field = valuePair.getField();
-            if (field.getDeb822KnownField() != null) {
-                // Already known, no need to check for typos
-                continue;
-            }
-            matches.clear();
-            String fieldName = field.getFieldName().toLowerCase();
-            if (knownFieldTable.getAutoStripXPrefix()) {
-                // Strip the "X-" prefix to avoid it hiding a match (e.g. "XC-Pakcage-Type" should be detected as Package-Type)
-                String stripped = KnownFieldTable.withXPrefixStripped(fieldName);
-                if (stripped != null) {
-                    fieldName = stripped;
-                }
-            }
-            for (String knownFieldName : knownFieldNames) {
-                int distance = EDIT_DISTANCE.apply(fieldName, knownFieldName);
-                if (distance < 0) {
-                    continue;
-                }
-                matches.add(knownFieldName);
-            }
-            if (!matches.isEmpty()) {
-                holder.registerProblem(
-                        field,
-                        Deb822Bundle.message("deb822.files.inspection.dctrl-misspelled-fields.description"),
-                        ProblemHighlightType.WARNING,
-                        matches.stream().map(n -> {
-                            Deb822KnownField knownField = knownFieldTable.getField(n);
-                            assert knownField != null;
-                            return AnnotatorUtil.replaceFieldNameFix(
-                                    Deb822Bundle.message("deb822.files.quickfix.rename-field.name", knownField.getCanonicalFieldName()),
-                                    Deb822Bundle.message("deb822.files.quickfix.rename-field.familyName"),
-                                    // requireNonNull is basically an assertion
-                                    knownField.getCanonicalFieldName()
-                            );
-                        }).toArray(LocalQuickFix[]::new)
-
                 );
             }
         }
